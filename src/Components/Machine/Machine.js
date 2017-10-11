89a514c93ab15e7a68c3788737987f5c526db56c
Wietse Neven <hello@wietseneven.com>
2017-10-11 12:21:03 +0200

    Preventing duplicate generates

--- src/Components/Machine/Machine.js
+++ src/Components/Machine/Machine.js
@@ -15,6 +15,11 @@ class Machine extends Component {
       mechanic: '',
       going: false,
     };
+
+    this.goneSubjects = [];
+    this.goneGroups = [];
+    this.goneMoments = [];
+    this.goneMechanics = [];
   }
 
   componentDidMount() {
@@ -37,21 +42,47 @@ class Machine extends Component {
     const amount = 100;
     const delay = 25;
     for (let i = 0; i < amount; i += 1) {
-      setTimeout(this.generate, delay * i);
+      this.randomTimeout = setTimeout(this.generate, delay * i);
     }
     setTimeout(() => {
+      clearTimeout(this.randomTimeout);
+      this.setFinal();
       this.setState({ going: false });
     }, amount * delay);
   };
 
   generate = () => {
-    const { subjects, groups, moments } = data;
+    const { subjects, groups, moments, mechanics } = data;
     const subject = subjects[Math.floor(Math.random() * subjects.length)];
     const group = groups[Math.floor(Math.random() * groups.length)];
     const moment = moments[Math.floor(Math.random() * moments.length)];
-    const mechanic = moments[Math.floor(Math.random() * moments.length)];
+    const mechanic = mechanics[Math.floor(Math.random() * mechanics.length)];
     this.setState({ subject, group, moment, mechanic });
   };
+
+  setFinal() {
+    const { subjects, groups, moments, mechanics } = data;
+    if (this.goneSubjects.length === subjects.length) this.goneSubjects = [];
+    if (this.goneGroups.length === subjects.length) this.goneGroups = [];
+    if (this.goneMoments.length === subjects.length) this.goneMoments = [];
+    if (this.goneMechanics.length === subjects.length) this.goneMechanics = [];
+
+    this.setState({
+      subject: this.getRandomWithManyExclusions(subjects, this.goneSubjects),
+      group: this.getRandomWithManyExclusions(groups, this.goneGroups),
+      moment: this.getRandomWithManyExclusions(moments, this.goneMoments),
+      mechanic: this.getRandomWithManyExclusions(mechanics, this.goneMechanics)
+    });
+  }
+
+  getRandomWithManyExclusions(originalArray, arrayOfIndexesToExclude) {
+    let rand = null;
+    while (rand === null || arrayOfIndexesToExclude.includes(rand)) {
+      rand = Math.round(Math.random() * (originalArray.length - 1));
+    }
+    this.goneSubjects.push(rand);
+    return originalArray[rand];
+  }
 
   reset = () => {
     this.setState({
