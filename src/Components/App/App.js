0ab5b893b3bf8ac11ef5c45d3cb9e58548c2ff50
Wietse Neven <hello@wietseneven.com>
2017-11-15 12:51:48 +0100

    Added ability to combine a team

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -7,6 +7,15 @@ import './App.css';
 import Machine from '../Machine';
 
 class App extends Component {
+  static shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      let temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+  }
+
   constructor() {
     super();
     this.state = {
@@ -17,7 +26,28 @@ class App extends Component {
 
   componentDidMount() {
     const id = this.props.match.params.generatorId;
-    const itemData = (data[id]) ? data[id] : { title: "Not found", slots: [{"title": "Found?", "items": ["Not found", "Doesn't exist", "Next time better."]}] };
+    const itemData = (data[id]) ? data[id] : {
+      title: "Not found",
+      slots: [{ "title": "Found?", "items": ["Not found", "Doesn't exist", "Next time better."] }]
+    };
+
+    if (itemData.type === "combiner") {
+      const slots = [];
+      // first put all the items in a random order
+      App.shuffleArray(itemData.items);
+
+      let i, j, k = 0, temparray, chunk = itemData.size;
+      for (i = 0, j = itemData.items.length; i < j; i += chunk) {
+        k += 1;
+        temparray = itemData.items.slice(i, i + chunk);
+        slots.push({
+          title: `Lid ${k}`,
+          items: temparray
+        })
+      }
+
+      itemData.slots = slots;
+    }
     this.setState({ data: itemData });
   }
 
